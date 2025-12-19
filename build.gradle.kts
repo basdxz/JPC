d4491f85036ff23a59f1b15f2edfdfe6bcf8db48
Ven <tudurap.com@gmail.com>
2025-12-19 18:04:38 +0000

    Building and publishing with gradle

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+    id("maven-publish")
+}
+
+group = "org.jpc"
+version = "3.0"
+
+java.toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+
+java {
+    withSourcesJar()
+}
+
+sourceSets.main {
+
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "org.jpc"
+            artifactId = "jpc"
+            from(components["java"])
+        }
+    }
+    repositories {
+        maven {
+            name = "venmaven"
+            setUrl("https://mvn.ventooth.com/releases")
+            credentials(PasswordCredentials::class)
+        }
+    }
+}
